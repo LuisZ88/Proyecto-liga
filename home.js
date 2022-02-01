3051de0c3b3f50e6effb8773f29681ba79b9064f
Luis <acosta88co@gmail.com>
2022-02-01 11:11:59 +0100

    inicio

---
+++ home.js
@@ -0,0 +1,10 @@
+
+
+// let partidos = data2.matches
+console.log(clasificacion)
+console.log(clasificacion.standings[0].table[0].draw)
+
+// for (let i = 0; i < clasificacion.standings[0].table.length; i++) {
+//     console.log(clasificacion.standings[0].table[i].team)
+//     // console.log(clasificacion.standings[0].table[i].draw)    
+// }
